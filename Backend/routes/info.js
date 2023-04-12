0f2c9ee6e64efd6df33693067b8b43ff483dd07e
spdanda <saipravallikadanda@gmail.com>
2023-04-12 12:42:48 +0530

    Updated info.js with Search function

--- Backend/routes/info.js
+++ Backend/routes/info.js
@@ -15,26 +15,32 @@ router.post("/",async(req,res)=>{
 });
 // Get Info list or Search Info by rfid or Infoid query parameters
 router.get("/", async (req, res) => {
-    const infoId = req.query.infoId;
-    const vehicleNumber = req.query.vehicleNumber;
+    //const infoId = req.query.infoId;
+    const vehicleNumberSc = req.query.vehicleNumber;
 
 
     /*fucntion to search a particular vehicle number */
 
 
-    // if(infoId || vehicleNumber){
-    //     try{
-    //         let Info;
-    //         if (infoId && vehicleNumber){
-    //             Info= await Info.find({
-    //                 infoId: infoId,
-    //                 vehicleNumber: vehicleNumber
-    //             });
-    //         } else if(infoId){
+    if(vehicleNumberSc){
+        try{
+            let info;
+            // if (infoId && vehicleNumber){
+            //     Info= await Info.find({
+            //         infoId: infoId,
+            //         vehicleNumber: vehicleNumber
+            //     });
+            // } else if(infoId){
 
-    //         }
-    //     }
-    // }
+            // }
+            if(vehicleNumberSc){
+                info = await Info.find({vehicleNumber : vehicleNumberSc});
+            }
+            return res.status(200).json(info);
+        } catch(error){
+            return res.status(500).json({error: error});
+        }
+    }
 })
 // Get Info by ID
 
